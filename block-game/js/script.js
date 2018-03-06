2f79ebdab7012210478f2e160ce9fdb7ce1dd321
nickpapadakis-matc <papadakn@gmatc.matc.edu>
2018-03-05 18:10:11 -0600

    working block game

--- block-game/js/script.js
+++ block-game/js/script.js
@@ -36,8 +36,26 @@ name: '6',
 selector: 'b6',
 bColor: 'yellow',
 color: 'black'
-}
-]
+},
+{
+  name:'7',
+  selector: 'b7',
+  bColor: 'rgb(134, 84, 51)',
+  color: 'black'
+},
+{
+  name:'8',
+  selector: 'b8',
+  bColor: 'rgb(105, 212, 255)',
+  color: 'black'
+},
+{
+  name:'9',
+  selector: 'b9',
+  bColor: 'rgb(117, 74, 159)',
+  color: 'white'
+}]
+
  var getEl = function(id){
    return document.getElementById(id);
 }
@@ -78,36 +96,52 @@ var win = function(){
   var chngColor = function(selector, id){
 
     switch(id){
-      case 0:{
+      case 1:{
         getEl(selector).style.backgroundColor = 'white';
         getEl(selector).style.color = 'black';
         break;
       }
-      case 1:{
+      case 2:{
         getEl(selector).style.backgroundColor = 'black';
         getEl(selector).style.color = 'white';
         break;
       }
-      case 2:{
+      case 3:{
         getEl(selector).style.backgroundColor = 'red';
         getEl(selector).style.color = 'white';
         break;
       }
-      case 3:{
+      case 4:{
         getEl(selector).style.backgroundColor = 'green';
         getEl(selector).style.color = 'white';
         break;
       }
-      case 4:{
+      case 5:{
         getEl(selector).style.backgroundColor = 'yellow';
         getEl(selector).style.color = 'black';
         break;
       }
-      case 5:{
+      case 6:{
         getEl(selector).style.backgroundColor = 'blue';
         getEl(selector).style.color = 'white';
         break;
       }
+      case 7:{
+        getEl(selector).style.backgroundColor = 'rgb(121, 175, 255)';
+        getEl(selector).style.color = 'white';
+        break;
+      }
+      case 8:{
+        getEl(selector).style.backgroundColor = 'rgb(42, 32, 124)';
+        getEl(selector).style.color = 'black';
+        break;
+      }
+      case 9:{
+        getEl(selector).style.backgroundColor = 'rgb(13, 214, 88)';
+        getEl(selector).style.color = 'black';
+        break;
+      }
+
     }
     getEl(selector).addEventListener('click', function(){
       var b1Col = getEl('b1').style.backgroundColor;
@@ -116,50 +150,43 @@ var win = function(){
       var b4Col = getEl('b4').style.backgroundColor;
       var b5Col = getEl('b5').style.backgroundColor;
       var b6Col = getEl('b6').style.backgroundColor;
+      var b7Col = getEl('b7').style.backgroundColor;
+      var b8Col = getEl('b8').style.backgroundColor;
+      var b9Col = getEl('b9').style.backgroundColor;
 
-      if(b1Col == b2Col && b1Col == b3Col && b1Col == b4Col && b1Col == b5Col && b1Col == b6Col){
+      if(b1Col == b2Col && b1Col == b3Col && b1Col == b4Col && b1Col == b5Col && b1Col == b6Col && b1Col == b7Col && b1Col == b8Col && b1Col == b9Col){
         win();
       }
     });
   }
 
   getEl('b1').addEventListener('click', function(){
-    chngColor('b1', getRandomInt(6));
+    chngColor('b1', getRandomInt(9));
   })
   getEl('b2').addEventListener('click', function(){
-    chngColor('b2', getRandomInt(6));
+    chngColor('b2', getRandomInt(9));
   })
   getEl('b3').addEventListener('click', function(){
-    chngColor('b3', getRandomInt(6));
+    chngColor('b3', getRandomInt(9));
   })
   getEl('b4').addEventListener('click', function(){
-    chngColor('b4', getRandomInt(6));
+    chngColor('b4', getRandomInt(9));
   })
   getEl('b5').addEventListener('click', function(){
-    chngColor('b5', getRandomInt(6));
+    chngColor('b5', getRandomInt(9));
   })
   getEl('b6').addEventListener('click', function(){
-    chngColor('b6', getRandomInt(6));
+    chngColor('b6', getRandomInt(9));
   })
-
-
-
-/*  getEl('b1').addEventListener('click', function(){
-    var b1Col = getEl('b1').style.backgroundColor;
-    var b2Col = getEl('b2').style.backgroundColor;
-    var b3Col = getEl('b3').style.backgroundColor;
-    var b4Col = getEl('b4').style.backgroundColor;
-    var b5Col = getEl('b5').style.backgroundColor;
-    var b6Col = getEl('b6').style.backgroundColor;
-
-    if(b1Col == b2Col && b1Col == b3Col && b1Col == b4Col && b1Col == b5Col && b1Col == b6Col){
-      win();
-    }
-  });*/
-
-
-
-
+  getEl('b7').addEventListener('click', function(){
+    chngColor('b7', getRandomInt(9));
+  })
+  getEl('b8').addEventListener('click', function(){
+    chngColor('b8', getRandomInt(9));
+  })
+  getEl('b9').addEventListener('click', function(){
+    chngColor('b9', getRandomInt(9));
+  })
 
 
 })();
